0b60367418004bdb4c95bd5c5bf457a24e1a6b1d
itaditya <adityaa803@gmail.com>
2017-01-19 13:01:03 +0530

    clubs:

--- js/main.js
+++ js/main.js
@@ -6,29 +6,30 @@ $(window).bind("load", function() {
         $(".preloader-wrapper").removeClass("visible");
         var textFields = $(".toi-text");
         var imgFields = $(".images-list img");
-        var activeImg = 0;
-        var activeText = 0;
-        var len = textFields.length;
-        $.each(imgFields, function(key, value) {
-            value.dataset.index = key;
-        })
-        $.each(textFields, function(key, value) {
-            value.dataset.index = key;
-        })
+        if (imgFields.length > 0 && textFields.length > 0) {
+            var activeImg = 0;
+            var activeText = 0;
+            var len = textFields.length;
+            $.each(imgFields, function(key, value) {
+                value.dataset.index = key;
+            });
+            $.each(textFields, function(key, value) {
+                value.dataset.index = key;
+            });
+            function changeImg() {
+                imgFields[activeImg].classList.remove("visible");
+                activeImg = (activeImg + 1) % len;
+                imgFields[activeImg].classList.add("visible");
+            }
 
-        function changeImg() {
-            imgFields[activeImg].classList.remove("visible");
-            activeImg = (activeImg + 1) % len;
-            imgFields[activeImg].classList.add("visible");
+            function changeText() {
+                textFields[activeText].classList.remove("visible");
+                activeText = (activeText + 1) % len;
+                textFields[activeText].classList.add("visible");
+            }
+            setInterval(changeText, 4000);
+            setInterval(changeImg, 4000);
         }
-
-        function changeText() {
-            textFields[activeText].classList.remove("visible");
-            activeText = (activeText + 1) % len;
-            textFields[activeText].classList.add("visible");
-        }
-        setInterval(changeText, 4000);
-        setInterval(changeImg, 4000);
         $("#menuModal").on('show.bs.modal', function() {
             console.log('test');
             $(".float-menu .hamburger").addClass("closed");
